6887175032217bdd9c3c1a660b4412c18c0b621c
Miles Hochstein <miles.hochstein@gmail.com>
2016-02-27 15:18:36 -0800

    final

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# cardShuffleAlgorithm-
+# cardShuffleAlgorithm-  miles.hochstein 
 
 This javascript randomly shuffles a deck of cards of arbitary size, from n = 3 to some large value.
 
@@ -6,3 +6,4 @@ Balances two constraints. (1) Sort array size (larger means fewer collisions) an
 
 You could test empirically to find sweet spot for sort array size.
 
+No guarantee that your random sort will not be identical to the original array, but this becomes very improbable with a 52 card deck.   For smaller arrays this would need to be checked.
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // A fast way to randomly shuffle array elements, 
-// representing a deck of cards of arbitrary size, but n > 3
-
-"use strict";
+// representing a deck of cards of arbitrary size
+ "use strict";
 
 //data
 var cards = ['0', '1','2','3','4','5','6','7','8','9',
@@ -10,37 +9,48 @@ var cards = ['0', '1','2','3','4','5','6','7','8','9',
 '30', '31','32','33','34','35','36','37','38','39',
 '40', '41','42','43','44','45','46','47','48','49',
 '50', '51'];
+
+// var cards = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,
+//              20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,
+//              40,41,42,43,44,45,46,47,48,49,50,51,'a','b'];
+
 //var cards = ['0', '1','2','3','4','5','6','7','8','9'];
 //var cards = ['0', '1','2'];
+//var cards = [0,1];
+
 
 //parameters
 var iterationCount=0;  //Useful to measure efficiency of alogrithm 
-var sortSpace =10;//Try setting multiplier of veryBigArraySize.length to maximize efficiency
-var veryBigArray = [];
-var veryBigArraySize = cards.length * sortSpace;
+var sortSpace =20;//Try setting multiplier of sortArraySize.length to maximize efficiency
+var sortArray = [];
+var sortArraySize = cards.length * sortSpace;
 
 	for ( var ii = 0;  ii<cards.length; ii++ ){
 		//create random index value for much bigger array
-		var randomIndex = Math.floor(Math.random() * veryBigArraySize);
+		var randomIndex = Math.floor(Math.random() * sortArraySize);
 		//distribute the existing array over the much bigger array
-		//taking care not to over-write existing card
+		//taking care not to over-write an existing card
 
-		if ( veryBigArray[randomIndex] === undefined ){
- 			console.log(' veryBigArray slot AVAILABLE', veryBigArray[randomIndex]);
- 			veryBigArray[randomIndex] = cards[ii];
- 			iterationCount= iterationCount +1;
+		if ( sortArray[randomIndex] === undefined ){
+ 			console.log(' sortArray slot AVAILABLE', sortArray[randomIndex]);
+ 			sortArray[randomIndex] = cards[ii]; 
+ 			iterationCount = iterationCount +1;
 
-		}
-		else{ console.log('veryBigArray slot NOT AVAILABLE',veryBigArray[randomIndex]);
-			ii = ii-1;  //decrement iterator to force repeat
+		} else { console.log('sortArray slot NOT AVAILABLE',sortArray[randomIndex]);
+			ii = ii-1;  //decrement iterator to force repeat for original card
 			iterationCount= iterationCount +1;
 		}
 	}
 
 	//filter undefined values
-	veryBigArray = veryBigArray.filter(function(n){ return n != undefined });
+	sortArray = sortArray.filter(function(n){ return n != undefined });
  
 //result
-console.log (veryBigArray);
-console.log("of length: ", veryBigArray.length);
+console.log (sortArray);
+	for ( var ii = 0;  ii<cards.length; ii++ ){
+		console.log("original: ", cards[ii], ' shuffled: ', sortArray[ii])
+	}
+
+console.log("of length: ", sortArray.length);
 console.log("iterationCount was: ", iterationCount);
+
