da1ddd8fc431c2e9f31b64ecb816bbd64070ba68
Miles Hochstein <miles.hochstein@gmail.com>
2016-02-27 14:54:32 -0800

    improvements

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cardShuffleAlgorithm-
+
+This javascript randomly shuffles a deck of cards of arbitary size, from n = 3 to some large value.
+
+Balances two constraints. (1) Sort array size (larger means fewer collisions) and (2) collision correction (which has some computational cost).
+
+You could test empirically to find sweet spot for sort array size.
+
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-//A fast way to randomly shuffle array elements
+// A fast way to randomly shuffle array elements, 
+// representing a deck of cards of arbitrary size, but n > 3
+
 "use strict";
-
-console.log( console.time(), ' is time start');
-
 
 //data
 var cards = ['0', '1','2','3','4','5','6','7','8','9',
@@ -11,16 +10,17 @@ var cards = ['0', '1','2','3','4','5','6','7','8','9',
 '30', '31','32','33','34','35','36','37','38','39',
 '40', '41','42','43','44','45','46','47','48','49',
 '50', '51'];
-var cards = ['0', '1','2','3','4','5','6','7','8','9'];
-// var cards = ['0', '1','2'];
+//var cards = ['0', '1','2','3','4','5','6','7','8','9'];
+//var cards = ['0', '1','2'];
 
 //parameters
-var sortSpace = 1;   //set multiplier of array.length to maximize efficiency
+var iterationCount=0;  //Useful to measure efficiency of alogrithm 
+var sortSpace =10;//Try setting multiplier of veryBigArraySize.length to maximize efficiency
 var veryBigArray = [];
 var veryBigArraySize = cards.length * sortSpace;
 
 	for ( var ii = 0;  ii<cards.length; ii++ ){
-		//create random index values for much bigger array
+		//create random index value for much bigger array
 		var randomIndex = Math.floor(Math.random() * veryBigArraySize);
 		//distribute the existing array over the much bigger array
 		//taking care not to over-write existing card
@@ -28,9 +28,12 @@ var veryBigArraySize = cards.length * sortSpace;
 		if ( veryBigArray[randomIndex] === undefined ){
  			console.log(' veryBigArray slot AVAILABLE', veryBigArray[randomIndex]);
  			veryBigArray[randomIndex] = cards[ii];
+ 			iterationCount= iterationCount +1;
+
 		}
 		else{ console.log('veryBigArray slot NOT AVAILABLE',veryBigArray[randomIndex]);
 			ii = ii-1;  //decrement iterator to force repeat
+			iterationCount= iterationCount +1;
 		}
 	}
 
@@ -40,3 +43,4 @@ var veryBigArraySize = cards.length * sortSpace;
 //result
 console.log (veryBigArray);
 console.log("of length: ", veryBigArray.length);
+console.log("iterationCount was: ", iterationCount);
