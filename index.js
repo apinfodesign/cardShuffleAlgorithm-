42817a66ef429282cb88e783c15b8c3a729eb701
Miles Hochstein <miles.hochstein@gmail.com>
2016-02-28 12:09:40 -0800

    ideal sort array size possibly 4 times

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // commented version
 // A fast way to randomly shuffle array elements, 
 // representing a deck of cards of arbitrary size
- "use strict";
-
+// Modify sortSpace to test performance.
+ 
 //data
 // var cards = ['0', '1','2','3','4','5','6','7','8','9',
 // '10', '11','12','13','14','15','16','17','18','19',
@@ -11,20 +11,26 @@
 // '40', '41','42','43','44','45','46','47','48','49',
 // '50', '51'];
 
+//52 cards
 var cards = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,
              20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,
              40,41,42,43,44,45,46,47,48,49,50,51];
+//104 cards
+//var cards = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51];             
 
 //var cards = ['0', '1','2','3','4','5','6','7','8','9'];
 //var cards = ['0', '1','2'];
 //var cards = [0,1];
 //var cards = ['ace','king','queen','jack','10','9'];
 
-console.time("dbsave");
+
+console.time("dbsave"); //start timing
 
 //parameters
 var iterationCount = 0;  //Useful to measure efficiency of alogrithm 
-var sortSpace = 10;
+//*************************
+var sortSpace = 4;   //Affects memory required for sort
+//*************************
 //Try setting sortSpace multiplier of sortArraySize.length to maximize efficiency and speed
 
 var shuffle = function(iterationCount, cards, sortSpace){
@@ -37,11 +43,12 @@ var shuffle = function(iterationCount, cards, sortSpace){
 		//taking care not to over-write an existing card
 
 		if ( sortArray[randomIndex] === undefined ){
- 			console.log(' sortArray slot AVAILABLE', sortArray[randomIndex]);
+ 			console.log(' sortArray available for', ii, ' at ', randomIndex, 'which is now', sortArray[randomIndex]);
  			sortArray[randomIndex] = cards[ii]; 
  			iterationCount = iterationCount +1;
 
-		} else { console.log('sortArray slot NOT AVAILABLE',sortArray[randomIndex]);
+		} else {  
+			console.log(' sortArray ***NOT AVAILABLE*** for', ii, ' at ', randomIndex, 'which is now', sortArray[randomIndex]);
 			ii = ii-1;  //decrement iterator to force repeat for original card
 			iterationCount = iterationCount +1;
 		}
